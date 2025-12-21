90ebf6b61e90de1aaf8051f6fbb87252c7901687
suresh <suresh123@gmail.com>
2025-12-21 11:56:21 +0530

    right and left half pyramid

--- IncresingOrDrecresingNoPyramid.java
+++ IncresingOrDrecresingNoPyramid.java
@@ -52,20 +52,102 @@ public void zeroOneTriangle(int n) {
 	 }
 }
 	public void palindromeTriange(int n) {
-		 for(int i=0; i<n; i++) {
+		 for(int i=1; i<n; i++) {
 			 for(int j=i; j<n; j++) {
 				 System.out.print(" ");
 			 }
 			 for(int k=i; k>1; k--) {
-				 System.out.print(  "{"+k+"}" );
+				 System.out.print(k +" ");
 			 }
 			 for(int v=1; v<=i; v++) {
-				 System.out.print(v+" ");
+				 System.out.print("`"+v+" ");
 			 }
 			 
 			 System.out.println();
 		 }
 	}
+	
+	
+	public void squareHollow(int n) {
+		  for(int i=0; i<=n; i++) {
+			   for(int j=0; j<=n; j++) {
+				   if(i==0 || j==0 || i== n|| j==n) {
+				  System.out.print("*");
+			   }else {
+				   System.out.print(" ");
+			   }
+				   }
+				   System.out.println();
+		  }
+	}
+	public void squareFill(int n) {
+		  for(int i=0; i<=n; i++) {
+			   for(int j=0; j<=n; j++) {
+				    System.out.print("*");
+				   }
+				   System.out.println();
+		  }
+	}
+	public void rhombus(int n) {
+		  for(int i=0; i<=n; i++) {
+			   for(int s=0; s<i ; s++) {
+				   System.out.print(" ");
+			   }
+			   for(int j=0; j<=n; j++) {
+				    System.out.print("*");
+				   }
+			   
+				   System.out.println();
+		  }
+	}
+	
+	// right and left half pyramid
+	public void rightHalfPyramid(int n ) {
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<i; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+	
+	public void rightReveHalfPyramid(int n ) {
+		for(int i=n; i>1; i--) {
+			for(int j=1; j<i; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+	public void leftHalfPyramid(int n ) {
+		for(int i=0; i<n; i++) {
+			for(int s=n; s>i; s--) {
+				System.out.print(" ");
+			}
+			for(int j =0; j <i ; j++) {
+				System.out.print("@");
+			}
+			System.out.println();
+		}
+	}
+	
+	
+	public void leftReveHalfPyramid(int n ) {
+		for(int i=0; i<n; i++) {
+			for(int s=0; s<=i; s++) {
+				System.out.print(" ");
+			}
+			for(int j=n; j>i; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+			
+			
+			
+	
+	}
+	
 	
 	
 }
--- PatternMain.java
+++ PatternMain.java
@@ -7,12 +7,20 @@ public class PatternMain {
 		IncresingOrDrecresingNoPyramid  obj = new  	IncresingOrDrecresingNoPyramid();
 //		obj.IncreasingNo(6);
 //		System.out.println("----------");
-//		obj.dcreasingNo(10);
-		
+//		obj.dcreasingNo(10);	
 //		obj.noChaningPramid(5);
 //		obj.noTringular(8);
 //		obj.zeroOneTriangle(6);
-		obj.palindromeTriange(6);
+//		obj.palindromeTriange(6);
+//		obj.squareHollow(5);
+//		obj.squareFill(5);
+//		obj.rhombus(5);
+//		obj.rightHalfPyramid(6);
+//		obj.rightReveHalfPyramid(6);
+//		obj.leftHalfPyramid(6);
+		obj.leftReveHalfPyramid(5);
+		
+		
 
 	}
 
